47ae767104a6fa345163f9782e6ce20abba16234
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-09-04 21:12:42 -0400

    Month and week parsing
    Parse data into R data format, parse by month and week, plot barplots over time
    
    Clean up code, reorganize where plots are located
    
    Allow for load of JSON file for quicker upstart

--- Clean/loadJSON.R
+++ Clean/loadJSON.R
@@ -1,7 +1,9 @@
 # This code loads in .JSON file and calculates initial descriptive statistics
 # to discern representativeness of the corpus.
 
-setwd("~/GitHub/Politics_of_Sermons/Clean")
+rm(list=ls())
+#setwd("~/GitHub/Politics_of_Sermons/Clean")
+setwd("C:/Users/Steve/Dropbox/PoliticsOfSermons")
 
 library(readtext)
 library(plyr)
@@ -10,14 +12,17 @@ library(ggplot2)
 library(ngram)
 
 # Read in .JSON of sermons and change variable names
-file <- 'C:/Users/sum410/Documents/GitHub/Politics_of_Sermons/Clean/sermon.JSON'
+#file <- 'C:/Users/sum410/Documents/GitHub/Politics_of_Sermons/Clean/sermon.JSON'
+file <- 'C:/Users/Steve/Dropbox/PoliticsOfSermons/sermon.JSON'
 serms <- readtext(file, text_field = 'sermonData')
 colnames(serms) <- c('doc_id', 'date', 'denom', 'title', 'sermon', 'author')
 
-# Remove duplicates (apparently none?)
+save(serms, file = 'sermsDF.R')
+load('sermsDF.R')
+
+# Remove duplicates
 deduped.serms <- serms[!duplicated(serms$sermon),]
-deduped.serms <- unique(serms)
-#deduped.serms <- serms[!duplicated(serms),]
+rm(serms)
 
 # Add year variable
 deduped.serms$year <- sapply(strsplit(deduped.serms$date, split=', ', 
@@ -31,9 +36,39 @@ year.group <- count(deduped.serms, "year")
 year.group$rel <- round(100 * year.group$freq / sum(year.group$freq),2)
 
 # Table of sermons by year
-stargazer(year.group, type ='latex', summary = FALSE, rownames = FALSE,
+stargazer(year.group, type = 'latex', summary = FALSE, rownames = FALSE,
           covariate.labels = c('Year', '# of Sermons', '% of Corpus'), 
-          column.sep.width = '10pt', digits=2)
+          column.sep.width = '10pt', digits = 2, header = FALSE)
+
+
+## Month
+# Convert dates to R style dates (this could be done above in liue of sapply())
+deduped.serms$date.conv <- as.Date(deduped.serms$date, '%b %d, %Y')
+
+# Parse month and group by month, save to new df
+deduped.serms$month <- as.Date(cut(deduped.serms$date.conv, breaks = "month"))
+month.group <- count(deduped.serms, 'month')
+month.group$relat <- round(100 * month.group$freq / sum(month.group$freq),2)
+
+# Table of sermons by month
+stargazer(month.group, type ='text', summary = FALSE, rownames = FALSE,
+          covariate.labels = c('Month', '# of Sermons', '% of Corpus'), 
+          column.sep.width = '10pt', digits=2, header = FALSE)
+# Plot by month
+ggplot(data=month.group, aes(x=month, y=freq)) +
+  geom_bar(stat="identity") # Make nicer
+
+
+## Week
+# Parse by week and group, save to new df
+deduped.serms$week <- as.Date(deduped.serms$date.conv, breaks = 'week')
+week.group <- count(deduped.serms, 'week')
+week.group$relat <- round(100 * week.group$freq / sum(week.group$freq),2)
+
+# Plot of sermons by week
+ggplot(data=week.group, aes(x=week, y=freq)) +
+  geom_bar(stat="identity") # Make nicer
+
 
 # Group number of sermons by denomination
 denom.group <- count(deduped.serms, 'denom')
@@ -42,7 +77,7 @@ denom.group$rel <- round(100 * denom.group$freq / sum(denom.group$freq),2)
 # Create table
 stargazer(denom.group, type ='latex', summary = FALSE, rownames = FALSE,
           covariate.labels = c('Denomination', '# of Sermons', '% of Corpus'), 
-          column.sep.width = '10pt', digits=2)
+          column.sep.width = '10pt', digits=2, header = FALSE)
 
 # Group number of sermons per pastor
 pastor.group <- count(deduped.serms, 'author')
