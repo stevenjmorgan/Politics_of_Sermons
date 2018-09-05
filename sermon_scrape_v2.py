5aa3210a5934caf4943562ac05665a1d4f07e0da
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-09-04 20:24:13 -0400

    Update front-end scraper

--- sermon_scrape_v2.py
+++ sermon_scrape_v2.py
@@ -43,18 +43,20 @@ sys.setdefaultencoding('utf8')
     return TEXT
 '''
 
+#time.sleep(10800)
+
 # Initiate web driver
 path_to_chromedriver = 'C:/Users/Steve/Desktop/chromedriver'
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 # Open initial webpage
 #url = 'https://www.sermoncentral.com/Sermons/Search/?CheckedScriptureBookId=&keyword=&denominationFreeText=&maxAge=&ref=AdvancedSearch-HomeSermon'
-url = 'https://www.sermoncentral.com/Sermons/Search/?page=1434&sortBy=Newest&keyword=&contributorId=&rewrittenurltype=&searchResultSort=Newest&CheckedScriptureBookId=&minRating=&maxAge=&denominationFreeText='
+url = 'https://www.sermoncentral.com/Sermons/Search/?page=4349&sortBy=Newest&keyword=&contributorId=&rewrittenurltype=&searchResultSort=Newest&CheckedScriptureBookId=&minRating=&maxAge=&denominationFreeText='
 #browser.get(url)
 
 serms = []
 elems = []
-counter = 23165
+counter = 78671
 #counter = 0
 
 # Set seed for pseudo-randomness
@@ -69,9 +71,9 @@ browser.get(url)
 ### Itereate through each search result page
 for i in range(0,10960): #10,960 or something crazy big
 
-    # Sleep program every fifth search result page for 2 minutes
-    #if i % 5 == 2:
-    #    time.sleep(120)
+    # Sleep program every fifth search result page for 12 seconds
+    if i % 5 == 4:
+        time.sleep(12)
 
     # Sleep program every search result page randomly from 1-10 sec.
     #time.sleep(random.randint(0,10))
@@ -92,6 +94,7 @@ for i in range(0,10960): #10,960 or something crazy big
     elems = [x for x in elems if not re.search('https://www.sermoncentral.com/pastors-preaching-articles/steven-fuller-finding-strength-when-you-don-t-feel-like-preaching-1225?ref=Footer', str(x.get_attribute("href")))]
     elems = [x for x in elems if not re.search('https://www.sermoncentral.com/pastors-preaching-articles/', str(x.get_attribute("href")))]
     elems = [x for x in elems if not re.search('/pastors-preaching-articles/', str(x.get_attribute("href")))]
+    elems = [x for x in elems if not re.search('/sermons/search', str(x.get_attribute("href")))]
 
 
     # Only retain every other link since each desired link appears twice as href
@@ -194,4 +197,3 @@ for i in range(0,10960): #10,960 or something crazy big
         elm.click()
     except:
         pass
-		
