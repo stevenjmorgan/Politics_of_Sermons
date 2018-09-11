c085ce58e4fbba3b0e65b825b9417c8e9c44fc41
Steven Morgan <sum410@psu.edu>
2018-09-11 09:46:23 -0400

    Change start
    Gonna have to go back to see what was missed temporally (due to changes in number of searches returned)

--- sermon_scrape_reverse_Nov2004.py
+++ sermon_scrape_reverse_Nov2004.py
@@ -42,7 +42,7 @@ sys.setdefaultencoding('utf8')
             )
     return TEXT
 '''
-time.sleep(10800)
+#time.sleep(10800)
 
 # Initiate web driver
 path_to_chromedriver = 'C:/Users/sum410/Desktop/chromedriver'
@@ -50,12 +50,12 @@ browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 # Open initial webpage
 #url = 'https://www.sermoncentral.com/Sermons/Search/?CheckedScriptureBookId=&keyword=&denominationFreeText=&maxAge=&ref=AdvancedSearch-HomeSermon'
-url = 'https://www.sermoncentral.com/Sermons/Search/?page=6969&sortBy=Newest&keyword=&contributorId=&rewrittenurltype=&searchResultSort=Newest&CheckedScriptureBookId=&minRating=&maxAge=&denominationFreeText='
+url = 'https://www.sermoncentral.com/Sermons/Search/?page=6915&sortBy=Newest&keyword=&contributorId=&rewrittenurltype=&searchResultSort=Newest&CheckedScriptureBookId=&minRating=&maxAge=&denominationFreeText='
 #browser.get(url)
 
 serms = []
 elems = []
-counter = 2022985
+counter = 2023796
 #counter = 0
 
 # Set seed for pseudo-randomness
