bafcc60d64c59067624082d43fd05a5f37f5a104
Steven Morgan <sum410@psu.edu>
2018-09-26 15:21:16 -0400

    Clean text data and save

---
+++ Exploratory/preprocess2[wip].py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 14:45:22 2018
+
+@author: sum410
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Cleans multiple documents of sermon corpus
+Created on Wed Sep 26 13:22:46 2018
+@author: sum410
+"""
+
+import os
+import glob
+import string
+import nltk
+#nltk.download()
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import re
+import pickle
+
+os.chdir('C:/Users/sum410/Dropbox/PoliticsOfSermons/Data/SampleLDA')
+
+# Empty list and read in files as elements again
+# Read in sample sermon data
+file_list = glob.glob(os.path.join(os.getcwd(),
+    "C:/Users/sum410/Dropbox/PoliticsOfSermons/Data/MasterList", "*.txt"))
+sample_serms = []
+for file_path in file_list[0:1000]:
+    with open(file_path, encoding="utf8") as f_input:
+        sample_serms.append(f_input.read()) # 3 sermons in list
+
+# Apply word_tokenize to each element of the list called incoming_reports
+tokenized_serms = [word_tokenize(doc) for doc in sample_serms]
+
+## Remove punctuation
+regex = re.compile('[%s]' % re.escape(string.punctuation))
+tokenized_serms_no_punctuation = []
+for serm in tokenized_serms:
+    new_serm = []
+    for token in serm: 
+        new_token = regex.sub(u'', token)
+        if not new_token == u'':
+            new_serm.append(new_token)
+    
+    tokenized_serms_no_punctuation.append(new_serm)
+
+print(len(tokenized_serms_no_punctuation))
+
+## Remove stop words
+tokenized_serms_no_stopwords = []
+for serm in tokenized_serms_no_punctuation:
+    new_term_vector = []
+    for word in serm:
+        if not word in stopwords.words('english'):
+            new_term_vector.append(word)
+    tokenized_serms_no_stopwords.append(new_term_vector)
+            
+print(len(tokenized_serms_no_stopwords))
+
+## Remove numbers
+nonstemmed_docs = []
+for serm in tokenized_serms_no_stopwords:
+    no_num_vector = []
+    for word in serm:
+        word = ''.join([i for i in word if not i.isdigit()])
+        if word != '':
+            no_num_vector.append(word)
+    nonstemmed_docs.append(no_num_vector)
+            
+#nonstemmed_docs = list(filter(None, nonstemmed_docs))
+print(len(nonstemmed_docs))
+
+## Stem words
+porter = PorterStemmer()
+stemmed_docs = []
+for doc in nonstemmed_docs:
+    final_doc = []
+    for word in doc:
+        final_doc.append(porter.stem(word))
+    stemmed_docs.append(final_doc)
+    
+print(len(stemmed_docs))
+
+# Delete intermediate lists
+del tokenized_serms_no_punctuation, tokenized_serms_no_stopwords
+
+# Save stemmed and non-stemmed docs 
+with open('saved_preprocess.pkl', 'wb') as f:  
+    pickle.dump([nonstemmed_docs, stemmed_docs], f)
--- Exploratory/sample_lda.py
+++ Exploratory/sample_lda.py
@@ -10,12 +10,13 @@ import glob
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import sys
+from importlib import reload
 import gensim
 from gensim import corpora
 import pickle
 
 reload(sys)
-sys.setdefaultencoding('utf8')
+#sys.setdefaultencoding('utf8') #Does not matter for python3
 
 os.chdir('C:/Users/sum410/Dropbox/PoliticsOfSermons/Data/SampleLDA')
 
@@ -24,25 +25,23 @@ file_list = glob.glob(os.path.join(os.getcwd(),
     "C:/Users/sum410/Dropbox/PoliticsOfSermons/Data/MasterList", "*.txt"))
 sample_serms = []
 for file_path in file_list[1:5]:
-    with open(file_path) as f_input:
+    with open(file_path, encoding="utf8") as f_input:
         sample_serms.append(f_input.read())
 
-print type(sample_serms[0])
+print(type(sample_serms[0]))
 
 # Pre-process data
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
-
-import numpy as np
 from numpy import array
 
-serms = array.empty(5)
+'''serms = array.empty(5)
 serms['text'] = array(sample_serms)
 
 sys.exit()
 
-serms['wordcount'] = serms.apply(lambda x: len(str(x).split(" ")))
+serms['wordcount'] = serms.apply(lambda x: len(str(x).split(" ")))'''
 
 
 '''
@@ -52,18 +51,23 @@ lemma = WordNetLemmatizer()
 def clean(doc):
     stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
     punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
-    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    num_free = ''.join([j for j in punc_free if not j.isdigit()])
+    no_small = ''.join([w for w in num_free if len(w)>2])
+    normalized = " ".join(lemma.lemmatize(word) for word in no_small.split())
+    #no_small = ' '.join([w for w in normalized if len(w)>3])
+    #no_small = [w for w in normalized if len(w) < 3]
     return normalized
 
 doc_clean = [clean(doc).split() for doc in sample_serms]
 #print type(sample_serms[1])
-print doc_clean
+print(doc_clean)
+'''
 
 #sample_serms = [preprocessing(x) for x in sample_serms]
 
-sys.exit()'''
+#sys.exit()
 
-'''
+
 # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
 dictionary = corpora.Dictionary(doc_clean)
 doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
@@ -71,11 +75,10 @@ doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
 Lda = gensim.models.ldamodel.LdaModel
 ldamodel = Lda(doc_term_matrix, num_topics=5, id2word = dictionary, passes=999)
 print(ldamodel.print_topics(num_topics=5, num_words=10))
-print '\n'
 
 # save the model to disk
-filename = 'finalized_model.sav'
-pickle.dump(ldamodel, open(filename, 'wb'))
+'''filename = 'finalized_model.sav'
+pickle.dump(ldamodel, open(filename, 'wb'))'''
 
 num_topics = 5
 topic_words = []
@@ -83,11 +86,11 @@ for i in range(num_topics):
     tt = ldamodel.get_topic_terms(i,20)
     topic_words.append([dictionary[pair[0]] for pair in tt])
 
-print topic_words[0]
-print topic_words[1]
-print topic_words[2]
-print topic_words[3]
-print topic_words[4]'''
+print(topic_words[0])
+print(topic_words[1])
+print(topic_words[2])
+print(topic_words[3])
+print(topic_words[4])
 
 
 # Vectorize data
@@ -105,9 +108,9 @@ lda = LatentDirichletAllocation(n_components=no_topics, max_iter=999, learning_m
 # Display topics
 def display_topics(model, feature_names, no_top_words):
     for topic_idx, topic in enumerate(model.components_):
-        print "Topic %d:" % (topic_idx+1)
-        print " ".join([feature_names[i]
-                        for i in topic.argsort()[:-no_top_words - 1:-1]])
+        print("Topic %d:" % (topic_idx+1))
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
 no_top_words = 10
 display_topics(lda, tf_feature_names, no_top_words)
