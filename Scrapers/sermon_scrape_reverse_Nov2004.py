7de4beb5dc42d36e939cb93b050c3fe0193960ab
Steven Morgan <sum410@psu.edu>
2018-10-03 16:47:12 -0400

    Update

--- Scrapers/sermon_scrape_reverse_Nov2004.py
+++ Scrapers/sermon_scrape_reverse_Nov2004.py
@@ -50,12 +50,12 @@ browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 # Open initial webpage
 #url = 'https://www.sermoncentral.com/Sermons/Search/?CheckedScriptureBookId=&keyword=&denominationFreeText=&maxAge=&ref=AdvancedSearch-HomeSermon'
-url = 'https://www.sermoncentral.com/Sermons/Search/?page=5924&sortBy=Newest&keyword=&contributorId=&rewrittenurltype=&searchResultSort=Newest&CheckedScriptureBookId=&minRating=&maxAge=&denominationFreeText='
+url = 'https://www.sermoncentral.com/Sermons/Search/?page=5845&sortBy=Newest&keyword=&contributorId=&rewrittenurltype=&searchResultSort=Newest&CheckedScriptureBookId=&minRating=&maxAge=&denominationFreeText='
 #browser.get(url)
 
 serms = []
 elems = []
-counter = 2037383
+counter = 2039344
 #counter = 0
 
 # Set seed for pseudo-randomness
