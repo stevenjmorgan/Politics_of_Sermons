a125d302bc3bcaba760fb0a08d6590f8438704ba
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-10-11 18:04:16 -0400

    Implement deep NN's

--- Models/baseline.py
+++ Models/baseline.py
@@ -9,6 +9,7 @@ import pandas as pd
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
 import time
+import sys
 
 # load the dataset
 data = open('C:/Users/Steve/Dropbox/PoliticsOfSermons/CNN/corpus', encoding="utf8").read()
@@ -18,6 +19,8 @@ for i, line in enumerate(data.split("\n")):
     labels.append(content[0])
     texts.append(content[1])
 print("Data read in")
+
+### Get CNN text and labels into pd df ###
 
 # create a dataframe using texts and lables
 trainDF = pd.DataFrame()
@@ -84,3 +87,63 @@ def create_model_architecture(input_size):
 classifier = create_model_architecture(xtrain_count.shape[1])
 accuracy = train_model(classifier, xtrain_count, train_y, xvalid_count, is_neural_net=True)
 print("NN, Count Vectors",  accuracy)
+
+sys.exit()
+
+### These require word embeddings
+# Convulutional Neural Network
+def create_cnn():
+    # Add an Input Layer
+    input_layer = layers.Input((70, ))
+
+    # Add the word embedding Layer
+    embedding_layer = layers.Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], trainable=False)(input_layer)
+    embedding_layer = layers.SpatialDropout1D(0.3)(embedding_layer)
+
+    # Add the convolutional Layer
+    conv_layer = layers.Convolution1D(100, 3, activation="relu")(embedding_layer)
+
+    # Add the pooling Layer
+    pooling_layer = layers.GlobalMaxPool1D()(conv_layer)
+
+    # Add the output Layers
+    output_layer1 = layers.Dense(50, activation="relu")(pooling_layer)
+    output_layer1 = layers.Dropout(0.25)(output_layer1)
+    output_layer2 = layers.Dense(1, activation="sigmoid")(output_layer1)
+
+    # Compile the model
+    model = models.Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy')
+    
+    return model
+
+classifier = create_cnn()
+accuracy = train_model(classifier, train_seq_x, train_y, valid_seq_x, is_neural_net=True)
+print("CNN, Word Embeddings",  accuracy)
+
+# RNN - LSTM
+def create_rnn_lstm():
+    # Add an Input Layer
+    input_layer = layers.Input((70, ))
+
+    # Add the word embedding Layer
+    embedding_layer = layers.Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], trainable=False)(input_layer)
+    embedding_layer = layers.SpatialDropout1D(0.3)(embedding_layer)
+
+    # Add the LSTM Layer
+    lstm_layer = layers.LSTM(100)(embedding_layer)
+
+    # Add the output Layers
+    output_layer1 = layers.Dense(50, activation="relu")(lstm_layer)
+    output_layer1 = layers.Dropout(0.25)(output_layer1)
+    output_layer2 = layers.Dense(1, activation="sigmoid")(output_layer1)
+
+    # Compile the model
+    model = models.Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy')
+    
+    return model
+
+classifier = create_rnn_lstm()
+accuracy = train_model(classifier, train_seq_x, train_y, valid_seq_x, is_neural_net=True)
+print("RNN-LSTM, Word Embeddings",  accuracy)
