188aa50cf7348addbda73a0891e8391bfb1cd9f1
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-10-11 17:51:26 -0400

    Implement shallow NN

--- Models/baseline.py
+++ Models/baseline.py
@@ -8,6 +8,7 @@ import numpy, string #xgboost, textblob
 import pandas as pd
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
+import time
 
 # load the dataset
 data = open('C:/Users/Steve/Dropbox/PoliticsOfSermons/CNN/corpus', encoding="utf8").read()
@@ -15,7 +16,8 @@ labels, texts = [], []
 for i, line in enumerate(data.split("\n")):
     content = line.split()
     labels.append(content[0])
-    texts.append(content[1:])
+    texts.append(content[1])
+print("Data read in")
 
 # create a dataframe using texts and lables
 trainDF = pd.DataFrame()
@@ -29,10 +31,12 @@ train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['t
 encoder = preprocessing.LabelEncoder()
 train_y = encoder.fit_transform(train_y)
 valid_y = encoder.fit_transform(valid_y)
+print('Data encoded')
 
 # create a count vectorizer object
 count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
-count_vect.fit(trainDF['text'])
+count_vect.fit_transform(trainDF['text'])
+print("Vectorized")
 
 # transform the training and validation data using count vectorizer object
 xtrain_count =  count_vect.transform(train_x)
@@ -49,3 +53,34 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid, i
         predictions = predictions.argmax(axis=-1)
 
     return metrics.accuracy_score(predictions, valid_y)
+
+# Naive Bayes on Count Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+print("NB, Count Vectors: ", accuracy)
+
+# Linear Classifier on Count Vectors
+accuracy = train_model(linear_model.LogisticRegression(), xtrain_count, train_y, xvalid_count)
+print("LR, Count Vectors: ", accuracy)
+
+# SVM on Ngram Level TF IDF Vectors
+accuracy = train_model(svm.SVC(), xtrain_count, train_y, xvalid_count)
+print("SVM, Count Vectors: ", accuracy)
+
+# Shallow neural network
+def create_model_architecture(input_size):
+    # create input layer 
+    input_layer = layers.Input((input_size, ), sparse=True)
+    
+    # create hidden layer
+    hidden_layer = layers.Dense(100, activation="relu")(input_layer)
+    
+    # create output layer
+    output_layer = layers.Dense(1, activation="sigmoid")(hidden_layer)
+
+    classifier = models.Model(inputs = input_layer, outputs = output_layer)
+    classifier.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy')
+    return classifier 
+
+classifier = create_model_architecture(xtrain_count.shape[1])
+accuracy = train_model(classifier, xtrain_count, train_y, xvalid_count, is_neural_net=True)
+print("NN, Count Vectors",  accuracy)
