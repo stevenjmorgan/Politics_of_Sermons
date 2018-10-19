e80fec49fa6eb467f1cef12f9796397fc060db47
Steven Morgan <sum410@psu.edu>
2018-10-19 13:14:35 -0400

    Clean sermons, locate pol speech

--- Clean/merge_pastor_sermon.R
+++ Clean/merge_pastor_sermon.R
@@ -6,6 +6,8 @@ setwd("C:/Users/sum410/Dropbox/PoliticsOfSermons")
 
 library(tm)
 library(qdap)
+library(dplyr)
+library(ggplot2)
 library(stringr)
 
 # Read in data on pastor profiles
@@ -17,29 +19,15 @@ dim(deduped.serms)
 # Merge pastor meta data into sermon df based on pastor name and denomination
 serms.merge <- merge(deduped.serms, pastors, by.x = c('author', 'denom'), 
                      by.y = c('name', 'denom'), all.x = TRUE)
-
 dim(serms.merge)
 
 # Too many rows
 #sum(!is.na(serms.merge$sermon))
 serms.merge <- serms.merge[!duplicated(serms.merge$sermon),]
-
 save(serms.merge, file = 'serms_merged.RData')
 
 ### Lexical level analysis ###
-
 # Create dictionary of explicitly political unigrams
-#political.words <- c('abortion', 'election', 'republican', 'democrat', 'vote', 
-#                     'gay', 'candidate', 'campaign', 'gop', 'politic',
-#                     'congress', 'poll', 'democracy', 'obama', 'clinton', 
-#                     'government', 'constituent', 'capitalism', 'socialism',
-#                     'liberal', 'conservative', 'beltway', 'healthcare', 
-#                     'LGBTQ', 'policy', 'Obamacare', 'partisan', 'welfare',
-#                     'progressive', 'economic', 'economy', 'constituent',
-#                     'senate', 'bill', 'bureaucracy', 'liberties', 
-#                     'constitution', 'Laissez-Faire', 'union', 'politics', 
-#                     'political')
-
 pat <- paste(c('abortion', 'republican', 'democrat', 'vote', 
                'candidate', 'campaign', 'gop', 'politic',
                'congress', 'poll', 'democracy', 'obama', 'clinton', 
@@ -52,19 +40,17 @@ pat <- paste(c('abortion', 'republican', 'democrat', 'vote',
                'same sex marriage', 'political'), collapse='|')
 #serms.merge$political <- grepl(pat, serms.merge$sermon_usable)
 
-
-test <- serms.merge[0:10,]
-test$sermon <- iconv(test$sermon, "latin1", "ASCII", sub="")
-test$sermon <- tolower(test$sermon)
-test$sermon <- gsub('[[:punct:]]+',' ', test$sermon)
-test$sermon <- gsub('[[:digit:]]+', ' ', test$sermon)
-stopwords('english') <- stopwords('english')[!170]
-test$sermon <- removeWords(test$sermon, stopwords('english'))
-test$sermon <- gsub('\\b\\w{1,2}\\b', ' ', test$sermon)
-test$sermon <- stripWhitespace(test$sermon)
-test$sermon <- trimws(test$sermon)
-
-test$sermon[1]
+#test <- serms.merge[0:10,]
+#test$sermon <- iconv(test$sermon, "latin1", "ASCII", sub="")
+#test$sermon <- tolower(test$sermon)
+#test$sermon <- gsub('[[:punct:]]+',' ', test$sermon)
+#test$sermon <- gsub('[[:digit:]]+', ' ', test$sermon)
+#stopwords('english') <- stopwords('english')[!170]
+#test$sermon <- removeWords(test$sermon, stopwords('english'))
+#test$sermon <- gsub('\\b\\w{1,2}\\b', ' ', test$sermon)
+#test$sermon <- stripWhitespace(test$sermon)
+#test$sermon <- trimws(test$sermon)
+#test$sermon[1]
 
 pattern <- paste(c('rejoice', 'grace'), collapse='|')
 test$find.word <- grepl(pattern, test$sermon)
@@ -80,6 +66,7 @@ serms.merge$sermon.clean <- gsub('\\b\\w{1,2}\\b', ' ', serms.merge$sermon.clean
 serms.merge$sermon.clean <- stripWhitespace(serms.merge$sermon.clean)
 serms.merge$sermon.clean <- trimws(serms.merge$sermon.clean)
 #serms.merge$sermon.clean[1]
+save(serms.merge, file = 'serms_cleaned_df.RData')
 
 # Search for political speech in cleaned sermons (non-stemmed)
 serms.merge$political <- grepl('abortion', serms.merge$sermon.clean)
@@ -88,12 +75,12 @@ serms.merge$social <- grepl('socialism', serms.merge$sermon.clean)
 summary(serms.merge$social)
 serms.merge$congress <- grepl('congress', serms.merge$sermon.clean)
 summary(serms.merge$congress)
-serms.merge$gay <- grepl('same sex marriages', serms.merge$sermon.clean)
+serms.merge$gay <- grepl('same sex marriage', serms.merge$sermon.clean)
 summary(serms.merge$gay)
+min(which(serms.merge$gay == TRUE))
+serms.merge$poll <- grepl('poll', serms.merge$sermon.clean)
 
-
-library(dplyr)
-library(ggplot2)
+# Visualize distribution of political speech across time
 tab_sum <- serms.merge %>% group_by(year) %>%
   filter(social) %>%
   summarise(trues = n())
