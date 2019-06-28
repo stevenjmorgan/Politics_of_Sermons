2058b6650f8d05540ad1756128fde6f7a321d9a0
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-06-28 13:01:37 -0400

    Update moral_foundations.R

--- Dissertation/moral_foundations.R
+++ Dissertation/moral_foundations.R
@@ -33,8 +33,12 @@ output_mfd <- liwcalike(serms$sermon[1:4], tolower = TRUE,
 
 mfd.df <- as.data.frame(matrix(nrow = length(serms$sermon), ncol = 28))
 colnames(mfd.df) <- colnames(output_mfd)
+mfd.scores <- output_mfd[1,]
+mfd.scores[1,] <- NA
 rm(output_mfd)
 for (i in 1:length(serms$sermon)) {
+  
+  mfd.scores[1,] <- NA
   
   try(
   # Calculate MFD score for sermon
@@ -51,3 +55,38 @@ for (i in 1:length(serms$sermon)) {
   mfd.df$docname[i] <- paste('sermon', as.character(i), sep = ' ')
   mfd.df$Segment[i] <- i
 }
+
+summary(is.na(mfd.df$fairness.vice))
+save(mfd.df, file = 'mfd.scores.RData')
+
+# Combine w/ sermon dataset
+serms <- cbind(serms, mfd.df)
+save(serms, file = 'sermons_mfd.RData')
+
+# Plot distributions
+hist(mfd.df$fairness.vice)
+hist(mfd.df$fairness.virtue)
+hist(mfd.df$care.virtue)
+hist(mfd.df$care.vice)
+hist(mfd.df$loyalty.virtue)
+hist(mfd.df$loyalty.vice)
+hist(mfd.df$authority.virtue)
+hist(mfd.df$authority.vice)
+hist(mfd.df$sanctity.virtue)
+hist(mfd.df$sanctity.vice)
+
+unique(serms$denom)
+
+hist(serms$care.virtue[which(serms$denom == 'Evangelical/Non-Denominational')],
+     xlab = 'Care - Virture', main = 'Evangelicals/Non-Denominational')
+hist(serms$care.virtue[which(serms$denom == 'Baptist')],
+     xlab = 'Care - Virture', main = 'Baptist')
+
+t.test(serms$care.virtue[which(serms$denom == 'Evangelical/Non-Denominational')], # sign.
+       serms$care.virtue[which(serms$denom == 'Baptist')])
+t.test(serms$care.vice[which(serms$denom == 'Evangelical/Non-Denominational')],
+       serms$care.vice[which(serms$denom == 'Baptist')])
+t.test(serms$fairness.virtue[which(serms$denom == 'Evangelical/Non-Denominational')], # sign.
+       serms$fairness.virtue[which(serms$denom == 'Baptist')])
+t.test(serms$fairness.vice[which(serms$denom == 'Evangelical/Non-Denominational')],
+       serms$fairness.vice[which(serms$denom == 'Baptist')])
