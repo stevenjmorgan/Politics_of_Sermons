7ca8eae4c510e8a59de6ced4fcd66156ced84fc3
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-07-30 15:30:48 -0400

    Update fightin_word_implementation.R

--- Dissertation/Fightin_Words/fightin_word_implementation.R
+++ Dissertation/Fightin_Words/fightin_word_implementation.R
@@ -12,6 +12,7 @@ devtools::install_github("matthewjdenny/SpeedReader")
 library(SpeedReader)
 library(stargazer)
 library(quanteda)
+library(readtext)
 
 load('final_dissertation_dataset7-27.RData')
 colnames(serms.merge)
@@ -28,10 +29,30 @@ memory.limit(size=56000)
 quanteda_dtm <- quanteda::dfm(serms.merge$clean,
                               #select = "[a-zA-Z]{3,}",
                               #valuetype = "regex")
-                              tolower=TRUE)#,
-                              #remove=c(stopwords(),",",".","-","\"","'","(",")",";",":"))
+                              tolower=TRUE,
+                              remove=c(",",".","-","\"","'","(",")",";",":"), #stopwords(),
+                              stem = TRUE)
 save(quanteda_dtm, file = 'huge_dtm7-30.RData')
 load('huge_dtm7-30.RData')
+
+
+# Corpus
+corp <- readtext(serms.merge, text_field = "clean")
+colnames(serms.merge)
+serms.merge$doc_id <- seq(1, nrow(serms.merge))
+serms.merge$text <- serms.merge$clean
+serms.merge <- serms.merge[,c(42,44,43,26,35,39,16)]
+serms.corpus <- corpus(serms.merge)
+summary(serms.corpus, 5)
+
+# Create DTM
+quanteda_dtm <- quanteda::dfm(serms.corpus,
+                              #select = "[a-zA-Z]{3,}",
+                              #valuetype = "regex")
+                              tolower=TRUE,
+                              remove=c(",",".","-","\"","'","(",")",";",":"), #stopwords(),
+                              stem = TRUE)
+
 
 # Convert to a slam::simple_triplet_matrix object
 dtm <- convert_quanteda_to_slam(quanteda_dtm)
@@ -43,13 +64,9 @@ doc_covariates <- serms.merge[,c('denom.fixed','date','state_parse','race',
                                  'gender.final')]
 
 
-## Create contingency table
-# Create a contingency table with all possible covariate combinations:
-cont_table <- contingency_table(metadata = doc_covariates,
-                                document_term_matrix = dtm)#,
-                                #variables_to_use = c(5))
 
-# Create dataframe for political/non-political
+# Create dataframe for male vs. female
+summary(serms.merge$word.count)
 gender <- as.data.frame(serms.merge[, c('gender.final')])
 colnames(gender) <- 'gender.final'
 gender$gender.final <- as.character(gender$gender.final)
@@ -64,6 +81,16 @@ cont.table <- contingency_table(metadata = gender,
 
 
 ## Run FW algorithm
+full.corp <- feature_selection(cont.table, 
+                               method = c("informed Dirichlet"),
+                               alpha = 0.01,
+                               rank_by_log_odds = F)
+fightin_words_plot(full.corp, positive_category = "Non-Political", 
+                   negative_category = "Political", 
+                   clean_publication_plots = FALSE,
+                   title = "Differences in Language: Political vs. Non-Political Tweets",
+                   display_top_words = 10,
+                   max_terms_to_display = 1e+10000)
 
 ## Plot results
 
