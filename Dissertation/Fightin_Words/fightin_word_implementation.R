bd8aef655b2517ead7189d467df57dc1c1836d88
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-07-30 14:47:42 -0400

    Update fightin_word_implementation.R

--- Dissertation/Fightin_Words/fightin_word_implementation.R
+++ Dissertation/Fightin_Words/fightin_word_implementation.R
@@ -18,11 +18,48 @@ colnames(serms.merge)
 
 #### Create function???
 
+
+
 ## Create dtm
+# Only keep tokens that are all letters and of len 3 or greater
+gc()
+memory.limit()
+memory.limit(size=56000)
+quanteda_dtm <- quanteda::dfm(serms.merge$clean,
+                              select = "[a-zA-Z]{3,}",
+                              valuetype = "regex")#,tolower=TRUE,
+                              #remove=c(stopwords(),",",".","-","\"","'","(",")",";",":"))
+save(quanteda_dtm, file = 'huge_dtm7-30.RData')
+
+# Convert to a slam::simple_triplet_matrix object
+dtm <- convert_quanteda_to_slam(quanteda_dtm)
 
 ## Create df for variable of interest
+# Covariate data
+colnames(serms.merge)
+doc_covariates <- serms.merge[,c('denom.fixed','date','state_parse','race',
+                                 'gender.final')]
+
 
 ## Create contingency table
+# Create a contingency table with all possible covariate combinations:
+cont_table <- contingency_table(metadata = doc_covariates,
+                                document_term_matrix = dtm)#,
+                                #variables_to_use = c(5))
+
+# Create dataframe for political/non-political
+gender <- as.data.frame(serms.merge[, c('gender.final')])
+colnames(gender) <- 'gender.final'
+gender$gender.final <- as.character(gender$gender.final)
+#gender$Gender[gender$Gender == 'male'] <- 'Political'
+#gender$Gender[gender$Gender == 'female'] <- 'Non-Political'
+
+# Create contigency table
+cont.table <- contingency_table(metadata = gender,
+                                document_term_matrix = dtm,
+                                force_dense = F)
+
+
 
 ## Run FW algorithm
 
