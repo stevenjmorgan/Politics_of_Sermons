58f3659cc2f5c6af60b8a8a40103f83cbe7851f1
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-07-29 20:10:00 -0400

    Update map_plots.R

--- Dissertation/Descriptives/map_plots.R
+++ Dissertation/Descriptives/map_plots.R
@@ -1,7 +1,8 @@
 # This script creates maps of church locations in the sermon dataset.
 
 rm(list=ls())
-setwd("C:/Users/steve/Dropbox/PoliticsOfSermons")
+setwd("C:/Users/steve/Dropbox/Dissertation/Data")
+
 
 library(zipcode)
 library(tidyverse)
@@ -10,37 +11,78 @@ library(viridis)
 library(ggthemes)
 library(albersusa)#installed via github
 
-load('final_serms.RData')
+load('final_dissertation_dataset7-27.RData')
 
-setwd("C:/Users/steve/Dropbox/Dissertation/Data")
 
 # Extract zip codes
-serms.merge$zip[1]
+#serms.merge$zip[1]
 serms.merge$zip.clean <- clean.zipcodes(serms.merge$zip)
 serms.merge$zip.clean[1]
 
 # Size by zip
+serms.merge$doc_id <- seq(1, nrow(serms.merge))
 fm.zip <- aggregate(data.frame(count=serms.merge$doc_id),list(zip=serms.merge$zip.clean),length)
 data(zipcode)
 fm <- merge(fm.zip, zipcode, by='zip')
 
 us <- map_data('state')
 
-# Plot map
+# Plot map -> raw sermon count
 ggplot(fm,aes(longitude,latitude)) +
   geom_polygon(data=us,aes(x=long,y=lat,group=group),color='gray',fill=NA,alpha=.35)+
   geom_point(aes(color = count),size=.15,alpha=.25) + labs(colour = "Count")  +
   xlim(-125,-65)+ylim(20,50) + xlab('Longitude') + ylab('Latitude') + 
   theme_bw()
-ggsave('sermons_by_zipcode.png', width = 6, height = 3.2)
+ggsave('sermons_state.png', width = 6, height = 3.2)
 
 
-library(plyr)
 
-pastor.group <- count(serms.merge, 'author')
-ggplot(pastor.group[which(pastor.group$freq < 35), ], aes(x=freq)) + 
-  geom_histogram(binwidth=1, color="darkblue", fill="lightblue") +
-  labs(x = 'Number of Sermons', y = "Pastors") + 
-  ggtitle("Distribution of Sermons Uploaded by Pastor") + theme_bw()
+install.packages(c("maps", "mapdata"))
 
-ggsave("sermonspastor.pdf")
+library(ggplot2)
+#library(ggmap)
+library(maps)
+library(mapdata)
+library(ggplot2); library(mapproj)
+library(tidyverse)
+
+##Name US State Map
+us <- map_data("state")
+
+##Read in Data; get rid of DC
+serms.merge.no.dc <- serms.merge[serms.merge$state_parse!="District of Columbia",]
+
+##Need to create "Region" variable with lowercase state names
+serms.merge.no.dc$state.lower <- tolower(serms.merge.no.dc$state_parse)
+group.state <- plyr::count(serms.merge.no.dc, 'state.lower')
+
+
+# Plot sermons by state
+ggplot() + geom_map(data=us, map=us, aes(long, lat, map_id=region), color="#2b2b2b", fill=NA, size=0.15) +
+  geom_map(data=group.state, map=us, aes(fill=freq, map_id=state.lower), color="#ffffff", size=0.15) + labs(x=NULL, y=NULL) +
+  scale_fill_continuous(low='gray80', high='gray20',  guide='colorbar') +
+  theme(panel.border = element_blank(), panel.background = element_blank(), 
+        plot.title = element_text(hjust = 0.5,size = 25, face = "bold"), axis.ticks = element_blank(),
+        axis.text = element_blank()) + guides(fill = guide_colorbar(title=NULL)) + coord_map()
+  #ggtitle("Number of Sermons by State")
+ggsave('sermons_state.png')
+
+
+
+#####################################################
+state.pop <- read.csv('C:/Users/steve/Desktop/sermon_dataset/state_pop.csv')
+state.pop$state.lower <- tolower(state.pop$State..federal.district..or.territory)
+test <- merge(group.state, state.pop, by = 'state.lower', all.x = TRUE)
+test <- test[,-3]
+test$prop <- test$freq/test$Population.estimate..July.1..2018.1.
+
+# Plot sermons per person by state
+ggplot() + geom_map(data=us, map=us, aes(long, lat, map_id=region), color="#2b2b2b", fill=NA, size=0.15) +
+  geom_map(data=test, map=us, aes(fill=prop, map_id=state.lower), color="#ffffff", size=0.15) + labs(x=NULL, y=NULL) +
+  scale_fill_continuous(low='gray80', high='gray20',  guide='colorbar') +
+  theme(panel.border = element_blank(), panel.background = element_blank(), 
+        plot.title = element_text(hjust = 0.5,size = 25, face = "bold"), axis.ticks = element_blank(),
+        axis.text = element_blank()) + guides(fill = guide_colorbar(title=NULL)) + coord_map() + 
+  ggtitle("Number of Sermons per Person by State")
+ggsave('sermons_person_state.png')
+
