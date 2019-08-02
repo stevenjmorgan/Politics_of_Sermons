4c24737bde6968155421e9592969d4c6628e3e24
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-08-02 16:21:45 -0400

    Update political_analysis.R

--- Dissertation/Political_Speech/political_analysis.R
+++ Dissertation/Political_Speech/political_analysis.R
@@ -1,6 +1,6 @@
 rm(list=ls())
-#setwd("C:/Users/steve/Desktop/sermon_dataset")
-setwd("C:/Users/sum410/Dropbox/Dissertation/Data")
+setwd("C:/Users/steve/Desktop/sermon_dataset")
+#setwd("C:/Users/sum410/Dropbox/Dissertation/Data")
 
 #serms <- read.csv('sermons_pol_variable7-31.csv')
 #summary(serms$pol_count)
@@ -15,13 +15,105 @@ nrow(serms.merge)
 serms.merge <- serms.merge[which(serms.merge$word.count > 75),]
 nrow(serms.merge)
 
+###############################################################################
 ### Political
 serms.merge <- read.csv('sermons_pol_variable7-31.csv', stringsAsFactors = F)
 colnames(serms.merge)
 
+summary(serms.merge$pol_count >= 5)
+pastors <- serms.merge[which(serms.merge$pol_count >= 5),]
+length(unique(pastors$author))
+length(unique(serms.merge$author))
+
+serms.merge$is.pol <- ifelse(serms.merge$pol_count >= 5, 1, 0)
+
+library(car)
+serms.merge$rel.trad <- recode(serms.merge$denom.fixed, 
+                               "'Evangelical/Non-Denominational' = 'evang'; 
+                               'Baptist' = 'evang'; 'Apostolic' = 'evang';
+                               'Lutheran' = 'mainline'; 
+                               'Christian/Church Of Christ' = 'evang';
+                               'Methodist' = 'mainline';
+                               'Independent/Bible' = 'other'; 
+                               'Pentecostal' = 'evang'; 'Nazarene' = 'evang';
+                               '*other' = 'other'; 
+                               'Presbyterian/Reformed' = 'mainline';
+                               'Bible Church' = 'evang'; 'Catholic' = 'cath';
+                               'Evangelical Free' = 'evang'; 
+                               'Holiness' = 'evang'; 'Adventist' = 'evang';
+                               'Assembly Of God' = 'evang';
+                               'Church Of God' = 'evang';
+                               'Christian Church' = 'other';
+                               'United Methodist' = 'mainline';
+                               'Christian Missionary Alliance' = 'evang';
+                               'Foursquare' = 'evang';
+                               'Seventh-Day Adventist' = 'evang';
+                               'Wesleyan' = 'evang';
+                               'Episcopal/Anglican' = 'mainline';
+                               'Orthodox' = 'other';
+                               'Charismatic' = 'evang';
+                               'Free Methodist' = 'evang';
+                               'Calvary Chapel' = 'evang';
+                               'Brethren' = 'evang';
+                               'Vineyard' = 'evang';
+                               'Mennonite' = 'evang';
+                               'Friends' = 'other';
+                               'Anglican' = 'mainline';
+                               'Congregational' = 'mainline';
+                               'Disciples Of Christ' = 'mainline';
+                               'Salvation Army' = 'evang';
+                               'Grace Brethren' = 'other';
+                               'Episcopal' = 'mainline';
+                               'Other' = 'other'; else = 'other'")
+
+unique(serms.merge$rel.trad)
+serms.merge$cath <- ifelse(serms.merge$rel.trad == 'cath', 1, 0)
+serms.merge$main <- ifelse(serms.merge$rel.trad == 'mainline', 1, 0)
+serms.merge$evang <- ifelse(serms.merge$rel.trad == 'evang', 1, 0)
+serms.merge$other <- ifelse(serms.merge$rel.trad == 'other', 1, 0)
+unique(serms.merge$race)
+serms.merge$black.final <- ifelse(serms.merge$race == 'black', 1, 0)
+serms.merge$hispanic.final <- ifelse(serms.merge$race == 'hispanic', 1, 0)
+serms.merge$white.final <- ifelse(serms.merge$race == 'white', 1, 0)
+serms.merge$api.final <- ifelse(serms.merge$race == 'api', 1, 0)
+
+colnames(serms.merge)
+unique(serms.merge$state_parse)
+
+unique(serms.merge$gender.final)
+serms.merge <- serms.merge[which(serms.merge$gender.final != ''),]
+serms.merge$gender.final <- ifelse(serms.merge$gender.final == 'female', 1, 0)
+
+# Census
+# Group states by Census region
+library(datasets)
+state.data <- as.data.frame(matrix(c(state.name, state.abb, as.character(state.region)), 
+                                   nrow = 50))
+temp <- as.data.frame(matrix(c("District of Columbia", "DC", "South"), 
+                             nrow = 1))
+state.data <- rbind(state.data, temp)
+colnames(state.data) <- c('name', 'abb', 'region')
+rm(temp)
+
+# Merge by Census region
+serms.merge <- merge(serms.merge, state.data, by.x = c('state_parse'), 
+                     by.y = c('name'))
+serms.merge$region <- as.character(serms.merge$region)
 
 
-####################
+# Models
+fit <- glm(is.pol~gender.final+black.final+hispanic.final+api.final+cath+
+             evang+other+region+as.factor(year), data = serms.merge,
+           family = "binomial")
+summary(fit)
+
+library(stargazer)
+stargazer(fit, dep.var.labels = 'Political Sermon', 
+          covariate.labels= c('Female','Black', 'Hispanic', 'Asian', 'Catholic','Evangelical',
+                              'Other','Northeast','South','West'))
+
+
+###############################################################################
 
 unique(serms.merge$author[which(serms.merge$gender.final == 'female')])
 
