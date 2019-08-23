ffc93e1bdaf90f4a614b47ed9bfc774a66c3c2c1
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-08-23 11:52:07 -0400

    Update religious_census.R

--- Dissertation/Chapter2/religious_census.R
+++ Dissertation/Chapter2/religious_census.R
@@ -4,13 +4,13 @@
 rm(list=ls())
 setwd("C:/Users/sum410/Dropbox/Dissertation/Data/Census")
 
-library(foreign)
-#library(xlsx)
+#library(foreign)
+library(xlsx)
 
-census2010 <- read.spss('U.S. Religion Census Religious Congregations and Membership Study, 2010 (County File).SAV', 
-                        to.data.frame=TRUE)
-#double.check <- read.xlsx('U.S. Religion Census Religious Congregations and Membership Study, 2010 (County File).xlsx',
-#                          sheetIndex = 1)
+#census2010 <- read.spss('U.S. Religion Census Religious Congregations and Membership Study, 2010 (County File).SAV', 
+#                        to.data.frame=TRUE)
+census2010 <- read.xlsx('U.S. Religion Census Religious Congregations and Membership Study, 2010 (County File).xlsx',
+                          sheetIndex = 1, stringsAsFactors = FALSE)
 
 # Total number of congregations (2010): All denominations/groups
 summary(census2010$TOTCNG)
@@ -24,3 +24,106 @@ sd(census2010$TOTADH, na.rm = T)
 summary(census2010$TOTRATE)
 sd(census2010$TOTRATE, na.rm = T)
 
+# State Abbreviation & Name
+unique(census2010$STABBR)
+unique(census2010$STNAME)
+
+# County Code & Name
+unique(census2010$CNTYCODE)
+unique(census2010$CNTYNAME)
+length(unique(census2010$CNTYCODE)) ### 327
+length(unique(census2010$CNTYNAME)) ### 1883
+
+# County Pop.
+summary(census2010$POP2010)
+
+# Create county-state variable
+census2010$county.state <- paste(census2010$CNTYNAME, census2010$STABBR, sep = ', ')
+unique(census2010$county.state)
+length(unique(census2010$county.state))
+
+
+#library(usmap)
+library(socviz)
+library(ggplot2)
+
+#county.data <- usmap::us_map(regions = "counties")
+#county.data$county.state <- paste(county.data$county, county.data$full, sep = ', ')
+
+# Merge religion census data w/ map df
+#dim(county.data)
+#county.data.merge <- merge(county.data, census2010, by = 'county.state', all.x = T)
+#dim(county.data.merge)
+
+library(ggthemes)
+library(tidyverse)
+county_full <- left_join(county_map, county_data, by = "id")
+county_full$county.state <- paste(county_full$name, county_full$state, sep = ', ')
+county.data.merge <- merge(county_full, census2010, by = 'county.state', all.x = T)
+summary(county.data.merge$TOTCNG)
+
+county.data.merge$TOTCNG[is.na(county.data.merge$TOTCNG)] <- min(county.data.merge$TOTCNG, na.rm = T)
+summary(county.data.merge$TOTCNG)
+
+### Raw # of congregations by county, 2010
+p <- ggplot(data = county.data.merge,
+            mapping = aes(x = long, y = lat,
+                          fill = TOTCNG, 
+                          group = group))
+p1 <- p + geom_polygon(color = "gray90", size = 0.05) + coord_equal()
+
+p2 <- p1 + scale_fill_gradient(low="lightblue",high="darkblue")
+p2 + labs(fill = "Congregations by County - 2010") +
+  theme_map() +
+  guides(fill = guide_legend(nrow = 1)) + 
+  theme(legend.position = "bottom")
+ggsave('raw_congregations_count_2010.png')
+
+
+### # of congregations divided by population by county, 2010
+county.data.merge$cong.by.pop <- county.data.merge$TOTCNG / county.data.merge$pop
+p <- ggplot(data = county.data.merge,
+            mapping = aes(x = long, y = lat,
+                          fill = cong.by.pop, 
+                          group = group))
+p1 <- p + geom_polygon(color = "gray90", size = 0.05) + coord_equal()
+p2 <- p1 + scale_fill_gradient(low="green",high="darkgreen")
+p2 + labs(fill = "Congregations Density (Normalized by County Population) - 2010") +
+  theme_map() +
+  guides(fill = guide_legend(nrow = 1)) + 
+  theme(legend.position = "bottom")
+ggsave('norm_congregations_count_2010.png')
+
+
+
+
+
+
+
+
+
+
+###############################################################################################################
+### Bad maps
+###############################################################################################################
+
+
+ggplot(data = county.data.merge,
+       mapping = aes(x = long, y = lat,
+                     fill = TOTCNG, 
+                     group = group)) + geom_polygon(color = "gray90", size = 0.05) + coord_equal()
+
+
+
+
+plot_usmap(regions = "counties", data = county.data.merge, values = "POP2010", lines = "blue") + 
+  scale_fill_continuous(name = "Congregations (2010)", label = scales::comma) + 
+  theme(legend.position = "right")
+
+plot_usmap(regions = "counties") + 
+  labs(title = "US Counties", subtitle = "This is a blank map of the counties of the United States.") + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"))
+
+plot_usmap(data = statepop, values = "pop_2015", lines = "red") + 
+  scale_fill_continuous(name = "Population (2015)", label = scales::comma) + 
+  theme(legend.position = "right")
