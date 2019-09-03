8c36995724be6d79f3f211e2d5a497218880d9a0
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-09-03 18:03:40 -0400

    Create sample_handcode.R

---
+++ Dissertation/Chapter2/sample_handcode.R
@@ -0,0 +1,11 @@
+### This script randomly samples 500 documents to hand code.
+
+rm(list=ls())
+#setwd("C:/Users/sum410/Dropbox/Dissertation/Data/Census")
+setwd("C:/Users/steve/Dropbox/Dissertation/Data/Census")
+
+load('C:/Users/sum410/Dropbox/Dissertation/Data/sermons_mfd_7-30.RData')
+
+serms.merge <- serms.merge[which(serms.merge$word.count > 100),]
+serms.merge$zip.clean[1:10]
+length(unique(serms.merge$zip.clean))
