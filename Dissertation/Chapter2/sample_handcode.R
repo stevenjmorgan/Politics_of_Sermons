e7a1406df370896a31460929fd88713bbe766711
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-09-03 18:39:22 -0400

    Update sample_handcode.R

--- Dissertation/Chapter2/sample_handcode.R
+++ Dissertation/Chapter2/sample_handcode.R
@@ -2,14 +2,24 @@
 
 rm(list=ls())
 #setwd("C:/Users/sum410/Dropbox/Dissertation/Data/Census")
-setwd("C:/Users/steve/Dropbox/Dissertation/Data/Census")
+setwd("C:/Users/steve/Dropbox/Dissertation/Data/handcode")
 
-load('C:/Users/steve/Dropbox/Dissertation/Data/model_sermons_subset.RData')
+gc()
+load('C:/Users/steve/Dropbox/Dissertation/Data/handcode/model_sermons_subset.RData')
+gc()
 
+# Check -> should not change
+dim(serms.merge)
 serms.merge <- serms.merge[which(serms.merge$word.count > 100),]
+dim(serms.merge)
+
 serms.merge$zip.clean[1:10]
 length(unique(serms.merge$zip.clean))
 
 # Sample
 set.seed(24519)
 smp <- serms.merge[sample(nrow(serms.merge),500),]
+
+smp$sermon[1]
+smp$clean[1]
+write.csv(smp, 'hand_code_sample.csv', row.names = F)
