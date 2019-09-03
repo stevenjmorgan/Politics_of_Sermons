c105264bdeddeb0e74cddbe9f7c4693f4d425caa
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-09-03 18:49:09 -0400

    Create hand_code_write.py

---
+++ Dissertation/Chapter2/hand_code_write.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 18:48:11 2019
+
+@author: steve
+"""
+
+import pandas as pd
+import os
+
+os.chdir('C:/Users/steve/Dropbox/Dissertation/Data/handcode')
+
+handcode = pd.read_csv('hand_code_sample.csv')
+handcode['clean'][0]
+
+os.chdir('C:/Users/steve/Dropbox/Dissertation/Data/handcode/sermons_handcode')
+
+for i in range(0,500):
+	file = 'hc_sermon' + str(i+1) + ".txt"
+
+	textFile = open(file, 'w')
+	textFile.write(handcode['clean'][i])
+	textFile.close()
