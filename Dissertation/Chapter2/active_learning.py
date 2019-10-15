8cd21c99c206674c7f7e00747dc41687291cd742
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-10-15 19:40:15 -0400

    Update active_learning.py

--- Dissertation/Chapter2/active_learning.py
+++ Dissertation/Chapter2/active_learning.py
@@ -5,7 +5,7 @@ Created on Tue Oct 15 19:13:35 2019
 @author: SF515-51T
 """
 
-import os
+import os, re, nltk, string
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
@@ -16,6 +16,27 @@ from sklearn import model_selection, preprocessing, linear_model, naive_bayes, m
 from sklearn import decomposition, ensemble
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
+
+def remove_punct(text):
+    text  = "".join([char for char in text if char not in string.punctuation])
+    text = re.sub('[0-9]+', '', text)
+    return text
+
+def tokenization(text):
+    text = re.split('\W+', text)
+    return text
+
+def remove_stopwords(text):
+    text = [word for word in text if word not in stopword]
+    return text
+
+def stemming(text):
+    text = [ps.stem(word) for word in text]
+    return text
+
+def remove_small_tokens(text):
+    text = [word for word in text if len(word) > 2]
+    return text
 
 def train_model(classifier, feature_vector_train, label, feature_vector_valid):
     # fit the training dataset on the classifier
@@ -30,9 +51,30 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid):
 os.chdir('C:/Users/SF515-51T/Desktop/Dissertation')
 
 # Read in hand labels
-coding = pd.read_csv('hand_code_sample_10-15_coded_no_text.csv', index_col = False)
+df = pd.read_csv('hand_code_sample_10-15_coded_no_text.csv', index_col = False)
 text = pd.read_csv('text.csv', index_col = False)
 
-coding['text'] = text['clean']
+df['text'] = text['clean']
 del(text)
+
+### Predict rights talk
+df['ground_truth_rights'].describe()
+df['ground_truth_rights'].value_counts() # 95 instances of rights talk
+
+df['text'][0]
+
+# Clean text
+df['clean'] = df['text'].apply(lambda x: remove_punct(x))
+df['clean'] = df['clean'].apply(lambda x: tokenization(x.lower()))
+stopword = nltk.corpus.stopwords.words('english')
+df['clean'] = df['clean'].apply(lambda x: remove_stopwords(x))
+ps = nltk.PorterStemmer()
+df['clean'] = df['clean'].apply(lambda x: stemming(x))
+df['clean'] = df['clean'].apply(lambda x: remove_small_tokens(x))
+df['clean'][0]
+#type(df['clean'][0])
+df['cleaned'] = df['clean'].apply(lambda x: ', '.join(map(str, x)))
+df['cleaned'] = df['cleaned'].str.replace(',', '')
+df['cleaned'][0]
+
 
