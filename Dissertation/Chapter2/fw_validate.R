61009e9c15c7f39cbad26d36431f389b0443d023
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-10-15 19:12:19 -0400

    Update fw_validate.R

--- Dissertation/Chapter2/fw_validate.R
+++ Dissertation/Chapter2/fw_validate.R
@@ -23,8 +23,14 @@ load('text_hand_sample.RData')
 df$text <- comb.df$clean
 rm(comb.df)
 
+# Remove words less than three characters
+for (i in 1:nrow(df)) {
+  df$cleaned[i] <- gsub('\\b\\w{1,2}\\b','',df$text[i])
+}
+df$cleaned[1]
+
 # Prepare data for FW algorithm -> unigrams
-quanteda_dtm <- quanteda::dfm(df$text, stem = T, tolower = T, remove = stopwords("english"),
+quanteda_dtm <- quanteda::dfm(df$cleaned, stem = T, tolower = T, remove = stopwords("english"),
                               verbose = T, remove_punct = TRUE) # , ngrams = 2
 
 # Convert to a slam::simple_triplet_matrix object
@@ -36,7 +42,7 @@ length(dtm$dimnames$Docs) == length(quanteda_dtm@Dimnames$docs)
 # Create dataframe for rights talk versus non-rights talk
 rights <- as.data.frame(df[, c('ground_truth_rights')])
 colnames(rights) <- 'Rights'
-rights[is.na(rights)] <- 0 ###
+summary(is.na(rights$Rights))
 
 # Create contigency table
 cont.table <- contingency_table(metadata = rights,
@@ -60,8 +66,8 @@ dev.off()
 ###############################################################################################
 ### Bigrams
 ###############################################################################################
-# Prepare data for FW algorithm -> unigrams
-quanteda_dtm <- quanteda::dfm(df$text, stem = T, tolower = T, remove = stopwords("english"),
+# Prepare data for FW algorithm -> unigrams and bigrams
+quanteda_dtm <- quanteda::dfm(df$cleaned, stem = T, tolower = T, remove = stopwords("english"),
                               verbose = T, remove_punct = TRUE, ngrams = 1:2)
 
 # Convert to a slam::simple_triplet_matrix object
@@ -94,6 +100,75 @@ fightin_words_plot(full.corp, positive_category = "Rights Talk",
 dev.off()
 
 
+### Bigrams only
+# Prepare data for FW algorithm -> bigrams only
+quanteda_dtm <- quanteda::dfm(df$cleaned, stem = T, tolower = T, remove = stopwords("english"),
+                              verbose = T, remove_punct = TRUE, ngrams = 2)
+
+# Convert to a slam::simple_triplet_matrix object
+dtm <- convert_quanteda_to_slam(quanteda_dtm)
+
+### Compare indices between slam matrix and dtm
+length(dtm$dimnames$Docs) == length(quanteda_dtm@Dimnames$docs)
+
+# Create dataframe for rights talk versus non-rights talk
+rights <- as.data.frame(df[, c('ground_truth_rights')])
+colnames(rights) <- 'Rights'
+rights[is.na(rights)] <- 0 ###
+
+# Create contigency table
+cont.table <- contingency_table(metadata = rights,
+                                document_term_matrix = dtm,
+                                force_dense = F)
+# Run FW algorithm
+full.corp <- feature_selection(cont.table,
+                               method = c("informed Dirichlet"),
+                               alpha = 0.01,
+                               rank_by_log_odds = F)
+png('rights_nonrights_hand_label_bigram_only.png', width=15,height=12,units="in",res=100)
+fightin_words_plot(full.corp, positive_category = "Rights Talk", 
+                   negative_category = "Non-Rights Talk", 
+                   clean_publication_plots = FALSE,
+                   title = "Differences in Language: Rights Talk vs. Non-Rights Talk Sermons",
+                   display_top_words = 20,
+                   max_terms_to_display = 1e+10000)
+dev.off()
+
+
+
+#### Trigrams
+# Prepare data for FW algorithm -> trigrams only
+quanteda_dtm <- quanteda::dfm(df$cleaned, stem = T, tolower = T, remove = stopwords("english"),
+                              verbose = T, remove_punct = TRUE, ngrams = 3)
+
+# Convert to a slam::simple_triplet_matrix object
+dtm <- convert_quanteda_to_slam(quanteda_dtm)
+
+### Compare indices between slam matrix and dtm
+length(dtm$dimnames$Docs) == length(quanteda_dtm@Dimnames$docs)
+
+# Create dataframe for rights talk versus non-rights talk
+rights <- as.data.frame(df[, c('ground_truth_rights')])
+colnames(rights) <- 'Rights'
+rights[is.na(rights)] <- 0 ###
+
+# Create contigency table
+cont.table <- contingency_table(metadata = rights,
+                                document_term_matrix = dtm,
+                                force_dense = F)
+# Run FW algorithm
+full.corp <- feature_selection(cont.table,
+                               method = c("informed Dirichlet"),
+                               alpha = 0.01,
+                               rank_by_log_odds = F)
+png('rights_nonrights_hand_label_trigrams.png', width=15,height=12,units="in",res=100)
+fightin_words_plot(full.corp, positive_category = "Rights Talk", 
+                   negative_category = "Non-Rights Talk", 
+                   clean_publication_plots = FALSE,
+                   title = "Differences in Language: Rights Talk vs. Non-Rights Talk Sermons",
+                   display_top_words = 20,
+                   max_terms_to_display = 1e+10000)
+dev.off()
 
 
 ##############################################################################################
