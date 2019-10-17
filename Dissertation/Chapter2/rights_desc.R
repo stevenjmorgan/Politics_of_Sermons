77d510249cfd9e7cca6a2cbd301f912b966c3707
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-10-17 19:35:51 -0400

    Create rights_desc.R

---
+++ Dissertation/Chapter2/rights_desc.R
@@ -0,0 +1,8 @@
+# Plot chapter 2 descriptives
+
+rm(list=ls())
+#setwd("C:/Users/sum410/Dropbox/Dissertation/Data/Census")
+#setwd("C:/Users/steve/Dropbox/Dissertation/Data/handcode")
+setwd("C:/Users/SF515-51T/Desktop/Dissertation")
+
+serms.rights <- read.csv('sermon_final_rights_ml.csv', stringsAsFactors = F)
