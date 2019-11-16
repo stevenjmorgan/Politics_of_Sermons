d8f8032a4ff193a2ba03b103d7721c6ebd3a9fd4
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-11-16 14:31:53 -0500

    Update fw_validate_attacks.R

--- Dissertation/Chapter2/fw_validate_attacks.R
+++ Dissertation/Chapter2/fw_validate_attacks.R
@@ -53,3 +53,65 @@ fightin_words_plot(full.corp, positive_category = "Attack on Religion Rhetoric",
 dev.off()
 
 
+############################################################################################3
+### Unigrams and bigrams
+# Prepare data for FW algorithm -> unigrams and bigrams
+quanteda_dtm <- quanteda::dfm(attacks$cleaned, stem = T, tolower = T, remove = stopwords("english"),
+                              verbose = T, remove_punct = TRUE, ngrams = 1:2)
+
+# Convert to a slam::simple_triplet_matrix object
+dtm <- convert_quanteda_to_slam(quanteda_dtm)
+
+### Compare indices between slam matrix and dtm
+length(dtm$dimnames$Docs) == length(quanteda_dtm@Dimnames$docs)
+
+
+# Create contigency table
+cont.table <- contingency_table(metadata = attack,
+                                document_term_matrix = dtm,
+                                force_dense = F)
+# Run FW algorithm
+full.corp <- feature_selection(cont.table,
+                               method = c("informed Dirichlet"),
+                               alpha = 0.01,
+                               rank_by_log_odds = F)
+png('attacks_nonattacks_hand_label_uni_bi.png', width=15,height=12,units="in",res=100)
+fightin_words_plot(full.corp, positive_category = "Attack on Religion Rhetoric", 
+                   negative_category = "Non-Attack Talk", 
+                   clean_publication_plots = FALSE,
+                   title = "Differences in Language: Attack on Religion vs. Non-Attack Rhetoric Sermons",
+                   display_top_words = 20,
+                   max_terms_to_display = 1e+10000)
+dev.off()
+
+
+############################################################################################3
+### Bigrams only
+# Prepare data for FW algorithm -> unigrams and bigrams
+quanteda_dtm <- quanteda::dfm(attacks$cleaned, stem = T, tolower = T, remove = stopwords("english"),
+                              verbose = T, remove_punct = TRUE, ngrams = 2)
+
+# Convert to a slam::simple_triplet_matrix object
+dtm <- convert_quanteda_to_slam(quanteda_dtm)
+
+### Compare indices between slam matrix and dtm
+length(dtm$dimnames$Docs) == length(quanteda_dtm@Dimnames$docs)
+
+
+# Create contigency table
+cont.table <- contingency_table(metadata = attack,
+                                document_term_matrix = dtm,
+                                force_dense = F)
+# Run FW algorithm
+full.corp <- feature_selection(cont.table,
+                               method = c("informed Dirichlet"),
+                               alpha = 0.01,
+                               rank_by_log_odds = F)
+png('attacks_nonattacks_hand_label_bi.png', width=15,height=12,units="in",res=100)
+fightin_words_plot(full.corp, positive_category = "Attack on Religion Rhetoric: Bigrams", 
+                   negative_category = "Non-Attack Talk", 
+                   clean_publication_plots = FALSE,
+                   title = "Differences in Language: Attack on Religion vs. Non-Attack Rhetoric Sermons",
+                   display_top_words = 18,
+                   max_terms_to_display = 1e+10000)
+dev.off()
