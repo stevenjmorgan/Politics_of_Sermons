cd9d54dca372a84af3084c7bb6e44b1216b1e2e8
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-11-17 15:43:03 -0500

    Update final_models.R

--- Dissertation/Chapter2/final_models.R
+++ Dissertation/Chapter2/final_models.R
@@ -266,11 +266,77 @@ stargazer(denom.rights,denom.attacks,denom.pol, dep.var.labels = c('Rights Talk'
           star.cutoffs = c(.05, .01),star.char = c("*", "**"))
 
 
+### Correlation table -> rights talk, attack on religion, political speech
+cor.test(serms.merge$rights_talk_xgboost, serms.merge$is.attack, use = 'complete.obs')
+cor.test(serms.merge$rights_talk_xgboost, serms.merge$is.pol, use = 'complete.obs')
+
+cor.table <- serms.merge[,c('rights_talk_xgboost', 'is.attack','is.pol')]
+colnames(cor.table) <- c('Rights Talk', 'Attack Religion', 'Political')
+mcor<-round(cor(cor.table),2)
+
+# Hide upper triangle
+upper<-mcor
+upper[upper.tri(mcor)]<-""
+upper<-as.data.frame(upper)
+upper
+
+
+library(xtable)
+print(xtable(upper), type="latex")
+
+library(Hmisc)
+
+corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper", "lower"),
+                    result=c("none", "html", "latex")){
+  #Compute correlation matrix
+  require(Hmisc)
+  x <- as.matrix(x)
+  correlation_matrix<-rcorr(x, type=method[1])
+  R <- correlation_matrix$r # Matrix of correlation coeficients
+  p <- correlation_matrix$P # Matrix of p-value 
+  
+  ## Define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .0001, "****", ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    "))))
+  
+  ## trunctuate the correlation matrix to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
+  
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
+  diag(Rnew) <- paste(diag(R), " ", sep="")
+  rownames(Rnew) <- colnames(x)
+  colnames(Rnew) <- paste(colnames(x), "", sep="")
+  
+  ## remove upper triangle of correlation matrix
+  if(removeTriangle[1]=="upper"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove lower triangle of correlation matrix
+  else if(removeTriangle[1]=="lower"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[lower.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove last column and return the correlation matrix
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  if (result[1]=="none") return(Rnew)
+  else{
+    if(result[1]=="html") print(xtable(Rnew), type="html")
+    else print(xtable(Rnew), type="latex") 
+  }
+} 
+
+corstars(cor.table, result="latex")
+
 
 ####################################################################################################################
 
-myvars <- c('gender.final', 'pop10', 'Parenth', 'census_region',
-            'pop_dens', 'pct_black', 'white.y', 'female', 'hh_income', 'su_gun4', 'TOTCNG', 
-            'TOTADH', 'TOTRATE', 'EVANCNG', 'EVANADH', 'EVANRATE', 'STNAME', 'dem.share', 'year', 'fair',
-            'cath', 'main', 'evang', 'other', 'black.final', 'hispanic.final', 'white.final', 'api.final',
-            'rights_talk_xgboost', 'comp.rescale')
+#myvars <- c('gender.final', 'pop10', 'Parenth', 'census_region',
+#            'pop_dens', 'pct_black', 'white.y', 'female', 'hh_income', 'su_gun4', 'TOTCNG', 
+#            'TOTADH', 'TOTRATE', 'EVANCNG', 'EVANADH', 'EVANRATE', 'STNAME', 'dem.share', 'year', 'fair',
+#            'cath', 'main', 'evang', 'other', 'black.final', 'hispanic.final', 'white.final', 'api.final',
+#            'rights_talk_xgboost', 'comp.rescale')
